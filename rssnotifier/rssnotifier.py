974ebd8654d7c6810cb07cc6ea1ba6bce2ee384e
Peter Schilling <peter@peterschilling.org>
2011-10-29 12:42:15 -0700

    for zack

--- rssnotifier/rssnotifier.py
+++ rssnotifier/rssnotifier.py
@@ -1,6 +1,6 @@
 import os
 import logging
-import re
+
 import feedparser
 
 from google.appengine.dist import use_library
@@ -17,105 +17,116 @@ from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import xmpp
 
 user_address = '' # put your email here to receive instant messages
+branch = 'zack'
+
+feed_urls = { 
+        'Vancouver': "http://vancouver.en.craigslist.ca/search/apa/van?query=&srchType=A&minAsk=800&maxAsk=1200&bedrooms=2&format=rss",
+        'Burnaby': "http://vancouver.en.craigslist.ca/search/apa/bnc?query=&srchType=A&minAsk=800&maxAsk=1200&bedrooms=2&format=rss",
+        'Slurry': "http://vancouver.en.craigslist.ca/search/apa/rds?query=&srchType=A&minAsk=800&maxAsk=1200&bedrooms=2&format=rss",
+        }
+
+illegal_words = [
+        "killarney",
+        "kits",
+        "south van",
+        "kerrisdale",
+        "abbotsford",
+        "comox",
+        "marpole",
+        "marple",
+        "rentersinvancouver.ca",
+        "basement",
+        "bsmt",
+        "oakridge",
+        "squamish",
+        "604-805-7278",
+        "yaletown",
+        "granville",
+        "dunbar",
+        ]
 
 class Post(db.Model):
     url = db.StringProperty(required=True)
     title = db.StringProperty(required=True)
     shithole = db.BooleanProperty()
+    hood = db.StringProperty()
     datetime = db.DateTimeProperty()
+    branch = db.StringProperty(default=branch)
 
 class Scrape(db.Model):
     start = db.DateTimeProperty()
     finish = db.DateTimeProperty()
     feed = db.StringProperty(required=True)
+    branch = db.StringProperty(default=branch)
 
 class ScrapeRequest(webapp.RequestHandler):
     def scrape(self):
-        feed_url = "http://vancouver.en.craigslist.ca/search/apa/van?query=&srchType=A&minAsk=800&maxAsk=1500&bedrooms=2&format=rss" 
-        scrape = Scrape(feed = feed_url, start = datetime.now())
+        for hood, feed_url in feed_urls.items():
+            scrape = Scrape(feed = feed_url, start = datetime.now())
 
-        illegal_words = [
-                "killarney",
-                "kits",
-                "south van",
-                "renfrew",
-                "kerrisdale",
-                "joyce",
-                "abbotsford",
-                "surrey",
-                "comox",
-                "marpole",
-                "marple",
-                "rupert",
-                "rentersinvancouver.ca",
-                "fraserview",
-                "basement",
-                "bsmt",
-                "oakridge",
-                "squamish",
-                "nanaimo",
-                "604-805-7278",
-                "yaletown",
-                "granville",
-                "dunbar",
-                "coquitlam",
-                "burnaby",
-                ]
+            feed = feedparser.parse(feed_url)
 
-        feed = feedparser.parse(feed_url)
+            im = ""
 
-        im = ""
+            for entry in feed.entries:
+                url = entry.link + "#!/branch=" + branch
 
-        for entry in feed.entries:
-            url = entry.link
+                existing = Post.get_by_key_name(url)
+                
+                if not existing:
 
-            existing = Post.get_by_key_name(url)
+                    date = parse(entry.date)
+                    post = Post(key_name = url,
+                            url = url, 
+                            title = entry.title, 
+                            shithole = False,
+                            datetime = date,
+                            hood = hood,
+                            )
+
+                    description = entry.title.lower() + ' ' + entry.description.lower()
+
+                    """
+                    I don't think zack is pretentious enough to eliminate
+                    streets from his search.
+
+                    # bug! only checks the first number found
+                    # e.g. it's available on the 3rd. 49th and main
+                    # get rid of anything past 32nd
+                    street = re.search(r"(\d+)(?:st|nd|rd|th)", description)
+
+                    if street:
+                        street_num = int(street.group(1))
+                        if street_num > 32 :
+                            post.shithole = True
+
+                    """
+
+                    for bad in illegal_words:
+                        shithole = description.find(bad)
+                        if (shithole != -1):
+                            post.shithole = True
+
+                    if not post.shithole:
+                        im = im + post.title + '\n' + post.url + '\n\n'
+
+                    # todo batch put
+                    post.put()
+
+            if len(im) > 0 and len(user_address) > 0:
+                chat_message_sent = False
+
+                if xmpp.get_presence(user_address):
+                    status_code = xmpp.send_message(user_address, im)
+                    chat_message_sent = (status_code == xmpp.NO_ERROR)
+                    logging.warning(`chat_message_sent` + ' ? ' + im)
+                else:
+                    xmpp.send_invite(user_address)
+                    logging.error('no instant message for you')
+
+            scrape.finish = datetime.now()
+            scrape.put()
             
-            if not existing:
-
-                date = parse(entry.date)
-                post = Post(key_name = url,
-                        url = url, 
-                        title = entry.title, 
-                        shithole = False,
-                        datetime = date,
-                        )
-
-                description = entry.title.lower() + ' ' + entry.description.lower()
-                # bug! only checks the first number found
-                # e.g. it's available on the 3rd. 49th and main
-                # get rid of anything past 32nd
-                street = re.search(r"(\d+)(?:st|nd|rd|th)", description)
-
-                if street:
-                    street_num = int(street.group(1))
-                    if street_num > 32 :
-                        post.shithole = True
-
-                for bad in illegal_words:
-                    shithole = description.find(bad)
-                    if (shithole != -1):
-                        post.shithole = True
-
-                if not post.shithole:
-                    im = im + post.title + '\n' + post.url + '\n\n'
-
-                # todo batch put
-                post.put()
-
-        if len(im) > 0 and len(user_address) > 0:
-            chat_message_sent = False
-
-            if xmpp.get_presence(user_address):
-                status_code = xmpp.send_message(user_address, im)
-                chat_message_sent = (status_code == xmpp.NO_ERROR)
-                logging.warning(`chat_message_sent` + ' ? ' + im)
-            else:
-                xmpp.send_invite(user_address)
-                logging.error('no instant message for you')
-
-        scrape.finish = datetime.now()
-        scrape.put()
 
     def get(self):
         scrape_time = None
@@ -147,14 +158,18 @@ class MainPage(webapp.RequestHandler):
         good_posts = db.GqlQuery("SELECT * "
                                 "FROM Post "
                                 "WHERE shithole = :1 "
+                                "AND branch = :2 "
                                 "ORDER BY datetime DESC LIMIT 20",
-                                False
+                                False, 
+                                branch
                                 )
         bad_posts = db.GqlQuery("SELECT * "
                                 "FROM Post "
                                 "WHERE shithole = :1 "
+                                "AND branch = :2 "
                                 "ORDER BY datetime DESC LIMIT 20",
-                                True
+                                True,
+                                branch
                                 )
 
 
